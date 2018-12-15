a70a146a4b5496bb97d9e8ca35a58b0304b84ca5
tekitou <tekitouraguna2@gmail.com>
2018-12-15 15:49:18 +0900

    テストコミット

---
+++ SampleLibrary.Tests/EvenDiscriminatorTest.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleLibrary.Tests
+{
+    [TestClass]
+    public class EvenDiscriminatorTest
+    {
+        [TestMethod]
+        [DataRow(2, true)]
+        [DataRow(3, false)]
+        [DataRow(10, true)]
+        [DataRow(11, false)]
+        public void DiscriminatorTest(int num ,bool expected)
+        {
+            var actual = EvenDiscriminator.Discriminate(num);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
